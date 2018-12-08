15a7578645b58e4140b7b901f6e66113c9a84ed4
Lucien Le Roux <lucien.le-roux@epitech.eu>
2018-12-08 16:47:29 -0500

    Put some text in homepage

--- demo/views/Home.vue
+++ demo/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
 	<div>
-		<h1>Home</h1>		
+		<h1>Home</h1>
+		<hr>
+		<h2>Installation</h2>
+		<code>npm install --save @shops/vue-components</code>
+		<hr>
+		<p><b>Version 0.1.5</b> Bunbled size: 9.5kB (CSS) + 12.3kB (JS)</p>
 	</div>
 </template>
+<style lang="scss" scoped>
+@import 'vars';
+
+code {
+	background-color: $grey-light;
+	padding: 4px;
+	font-weight: normal;
+}
+</style>
+
