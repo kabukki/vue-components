bbfa8dd041e4e49b10fb169fdce8ac088cc8fbb0
Lucien Le Roux <lucien.le-roux@epitech.eu>
2019-03-08 00:05:15 -0500

    Textarea: improved and added label + helper (fixes #7)

--- docs/views/Textarea.vue
+++ docs/views/Textarea.vue
@@ -6,12 +6,46 @@
 		<h2>Examples</h2>
 		<h3>Text</h3>
 		<KTextarea placeholder="Your text" v-model="text"/>
+		<KTextarea placeholder="Your text" disabled v-model="text"/>
+		<h3>With label</h3>
+		<KTextarea placeholder="Your text" label="This is a label" v-model="text"/>
+		<h3>With helper text</h3>
+		<KTextarea placeholder="Your text" help="This is a helper text!"  v-model="text"/>
 		<hr>
 		<h2>Usage</h2>
 		<p>TODO</p>
 		<hr>
 		<h2>Reference</h2>
-		<p>Nothing to show.</p>
+		<table>
+			<thead>
+				<tr>
+					<th>Property</th>
+					<th>Type</th>
+					<th>Default</th>
+					<th>Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>disabled</td>
+					<td><code>Boolean</code></td>
+					<td><code>false</code></td>
+					<td>disable the component</td>
+				</tr>
+				<tr>
+					<td>label</td>
+					<td><code>String</code></td>
+					<td><code>undefined</code></td>
+					<td>The input label or title</td>
+				</tr>
+				<tr>
+					<td>help</td>
+					<td><code>String</code></td>
+					<td><code>undefined</code></td>
+					<td>Adds a small helper text after the input</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </template>
 <script>
--- src/components/KTextarea.vue
+++ src/components/KTextarea.vue
@@ -1,12 +1,16 @@
 <template>
 	<div class="k-textarea">
+		<span class="label" v-if="label">{{ label }}</span>
 		<textarea v-bind="$attrs" :value="value" @input="onInput"></textarea>
+		<small class="help" v-if="help">{{ help }}</small>
 	</div>
 </template>
 <script>
 export default {
 	inheritAttrs: false,
 	props: {
+		label: String,
+		help: String,
 		value: {
 			type: String,
 			required: true
