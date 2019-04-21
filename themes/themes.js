4f413643fb6fe137d687993041896185764400bc
Lucien Le Roux <lucien.le-roux@epitech.eu>
2019-04-21 18:10:30 -0400

    docs: updated themes.js exports

--- themes/themes.js
+++ themes/themes.js
@@ -5,16 +5,17 @@
  * . class: css class to use to apply theme
  * . hasDarkMode: whether the theme implements a dark variant
  */
-module.exports = [
-	{
+module.exports = {
+	shops: {
 		name: 'Shops',
 		description: 'Shops Epitech Innovative Project',
 		className: 'shops',
 		hasDarkMode: true
-	}, {
+	},
+	alidhan: {
 		name: 'Alidhan',
 		description: 'Alidhan game',
 		className: 'alidhan',
 		hasDarkMode: false
 	}
-];
+};
