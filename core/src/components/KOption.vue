db2070c5274ccfad23d78ddcc0d3e8f6cff6917c
Lucien Le Roux <lucien.le-roux@epitech.eu>
2019-05-22 12:48:53 -0400

    Fixed KOption attrs

--- core/src/components/KOption.vue
+++ core/src/components/KOption.vue
@@ -5,29 +5,6 @@
 </template>
 <script>
 export default {
-	name: 'KOption',
-	inheritAttrs: true,
-	props: {
-		value: {
-			required: true
-		}
-	},
-	computed: {
-		listeners () {
-			return {
-				...this.$listeners,
-				onchange: this.onX,
-				input: this.onX,
-				click: this.onX,
-				select: this.onX,
-				check: this.onX
-			};
-		}
-	},
-	methods: {
-		onX (e) {
-			console.log(e);
-		}
-	}
+	name: 'KOption'
 };
 </script>
